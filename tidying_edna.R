90ed6f001eabf6a76dd0bca49f6ef05afd6ee8e8
Jenny Rogers <jbtaylor339@gmail.com>
2021-08-18 10:25:07 -0400

    finished tidying eDNA results

--- tidying_edna.R
+++ tidying_edna.R
@@ -28,7 +28,7 @@ names(dna2)[2] <- "species"
 names(dna2)[5] <- "meanCq"
 dna2$year <- 2021
 dna2 <- dna2 %>% 
-  select(site, pos, species, meanCq, year)
+ dplyr:: select(site, pos, species, meanCq, year)
 dna2$site <- ifelse(dna2$site == "PTB_t01", "PTB_T01",
                     ifelse(dna2$site == "PTB_t02", "PTB_T02", 
                            ifelse(dna2$site == "THB_t01", "THB_T01", dna2$site)))
@@ -57,15 +57,17 @@ dna_all$pos_new <- ifelse(dna_all$pos == "4/4", 1.0,
                              ifelse(dna_all$pos == "2/4", 0.50,
                                     ifelse(dna_all$pos == "1/4", 0.25, 0))))
 
+dna_all$pos[is.na(dna_all$pos)] <- 0
+dna_all$pos_new[is.na(dna_all$pos_new)] <- 0
 
 
 
 
 
-#join dna_all with dat for spatial information
+#fix file with the spatial information
 geo <- st_read("raw_data/EDNA.shp")
 geo <- geo %>% 
-  select(-1) %>% 
+  dplyr:: select(-1) %>% 
   rename("site" = "Site_Type")
 geo$site <- ifelse(geo$site == "MLB01", "MLB001",
                           ifelse(geo$site == "MLB02", "MLB002",
@@ -78,19 +80,45 @@ test <- dna_all %>%
   filter(!site %in% geo$site) #THBT03
 
 
-dat <- left_join(dat, dna_all, by = "site")
 
 
-dat <- dat %>% 
-  select(site, species, pos, pos_new, occurrence, Notes, geometry) %>% 
-  rename("replicates" = "pos")
-
-
+#first add the date 2020 to the spatial data so that we can join the notes in the spatial data to the correct site
+geo$year <- 2020
+t <- geo %>% 
+  dplyr:: select(site, Notes, year)
+dna_all <- left_join(dna_all, t, by = c("site", "year"))
 #notes in THB001 is incorrect
-dat[dat$site == "THB001", 6] <- NA
+dna_all[dna_all$site == "THB001", 11] <- NA
+
+#combine the notes column and remove the geometry
+dna_all$notes <- ifelse(is.na(dna_all$notes), dna_all$Notes, dna_all$notes)
+dna_all <- dna_all %>% 
+  dplyr:: select(-Notes, -geometry) %>% 
+  dplyr:: select(site, date, year, species, pos, pos_new, occurrence, meanCq, samples, notes)
+
+
+
+
+
+#add spatial data to dna_all
+geo <- geo %>% 
+  dplyr:: select(-Notes, -year) 
+dna_all <- left_join(geo, dna_all, by = "site")
+
+
+
+
+
+
+# NOTE: THBT03 was deleted from data because there was no spatial information - we could go in and add this manually (it was negative for brook trout)
+
+
+
+
+
 
 #write shapefile
-dat <- as(dat, "Spatial")
-shapefile(dat, file = "eDNA_spring2020_neponset.shp")
+final <- as(dna_all, "Spatial")
+shapefile(final, file = "eDNA_results.shp", overwrite = TRUE)
 
 edna <- st_read("eDNA_spring2020_neponset.shp")
